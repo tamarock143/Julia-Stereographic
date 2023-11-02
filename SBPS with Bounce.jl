c4a4882dadfbd18d919170eeb66e795b5b3e03a6
Cameron Bell <cameronbell143@gmail.com>
2023-11-02 16:39:34 +0000

    Something is happening which is causing the path to gradually leave the sphere

--- SBPS with Bounce.jl
+++ SBPS with Bounce.jl
@@ -3,7 +3,6 @@ include("Stereographic Projection.jl")
 include("Brent's Method.jl")
 using Random
 using Plots
-using StatsBase
 
 #We simulate an SBPS path targeting the disribtuion f. It requires x -> ∇log(f(x))
 #This requires bounce events and refreshment events
