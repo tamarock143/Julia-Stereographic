377fa19e9b44ed62ddcc65a9ee73aabeba3421a2
Cameron Bell (Uni Desktop) <cameronbell143@gmail.com>
2023-11-14 11:27:52 +0000

    Included my choice of adaptation times

--- Adaptive SBPS.jl
+++ Adaptive SBPS.jl
@@ -1,1 +1,11 @@
-#This will contain the Adaptive SBPS framework
+#Import SBPS Simulator
+include("SBPS with Bounce.jl")
+
+times = zeros(10)
+beta = 0.5
+
+for k in 1:10
+    times[k] = 2^findfirst(map(x -> 2^x, 0:10) .>= k^beta)
+end
+
+times
