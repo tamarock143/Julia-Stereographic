53279d10a9fb46917cd39817fcae5dfb5fa65bc4
Cameron Bell <cameronbell143@gmail.com>
2024-01-04 11:43:12 +0000

    Made the LeapFrog integrator

--- Hamiltonian MC.jl
+++ Hamiltonian MC.jl
@@ -3,11 +3,37 @@
 #Import LinearAlgebra library
 using LinearAlgebra
 
-HMC = function (gradlogf, x0, N::BigInt, deltat, L; M = I(length(x0)))
+#Leapfrog Integrator: take L leapfrog steps of length delta
+LeapFrog = function (gradlogf, x, p, delta, L; Minv = I(length(x)))
+    p += delta/2 * gradlogf(x) #First "half-update" for p
+
+    #We treat the final leapfrog separately, since it only needs a half-update for p
+    for i in 1:L-1
+        x += delta*Minv*p #Update x
+        p += delta*gradlogf(x) #Update p
+    end
+
+    x += delta*Minv*p #Update x
+    p += delta/2*gradlogf(x) #Final "half-update" for p
+
+    return (x = x, p = p)
+end
+
+#HMC Algorithm
+HMC = function (gradlogf, x0, N::BigInt, delta, L; M = I(length(x0)))
     d = length(x0) #The dimension
+    Minv = inv(M) #Invert M preemptively
+
     xout = zeros(N,d) #Prepare output
+    xout[1,:] = x0
 
-    p = sqrt(M)*randn(d) #Velocity vector, follows Normal(0,M)
+    x = x0 #Position vector, initialised at x0
+    p = zeros(d) #Velocity vector, will be reinitialised according to Normal(0,M) at each step
+
+    for n in 2:N
+        #Initialise velocity
+        p = sqrt(M)*randn(d)
 
 
+    end
 end
