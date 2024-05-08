30b2ab5f8be0e3e39d0731c33a8292c286fab1d3
Cameron Bell <cameronbell143@gmail.com>
2024-05-08 12:31:51 +0100

    Syncing laptop and desktop

--- MiscTests.jl
+++ MiscTests.jl
@@ -10,18 +10,17 @@ using SpecialFunctions
 using StatsBase
 using JLD
 
-d = 2
-d = 2
+d = 50
 sigma = sqrt(d)I(d)
-mu = zeros(d) .+1e8
+mu = zeros(d) .+ 1e4
 
-nu = 2
+nu = 50
 
 d > 1 ? x0 = sigma*normalize(randn(d)) + mu : x0 = (sigma*rand([1,-1]) + mu)[1]
 
-f = x -> -(nu+d)/2*log(nu+sum(x.^2))
+f = x -> -sum(x.^2)/2
 
-Nslice::Int64 = 1e4
+Nslice::Int64 = 1e5
 
 beta = 1.1
 burnin = 500
@@ -37,17 +36,16 @@ r = 1e-3
 p(x) = 1/sqrt(2pi)*exp(-x^2/2)
 #q(x) = 1/sqrt(2pi*sigmaf)*exp(-x^2/2sigmaf)
 q(x) = gamma((nu+1)/2)/(sqrt(nu*pi)*gamma(nu/2))*(1+x^2/nu)^-((nu+1)/2)
-b_range = range(-5,5, length=101)
+b_range = range(-10,10, length=101)
 
 histogram(sliceout.x[:,1], label="Experimental", bins=b_range, normalize=:pdf, color=:gray)
+stephist!(collect(b_range), label="Theoretical", bins=b_range, normalize =:pdf, weights= exp.(-abs.(b_range)), color=:red, lw=3)
 plot!(p, label= "N(0,1)", lw=3)
 plot!(q, label= "t", lw=3)
 xlabel!("x")
 ylabel!("P(x)")
 
-findfirst(sliceout.x .== NaN)
-
-plot(sliceout.x[:,1], label = "x1")
+plot(sliceout.x[501:end,1], label = "x1")
 vline!(cumsum(sliceout.times[1:end-1]), label = "Adaptations", lw = 0.5)
 
 plot(sliceout.z[:,end], label = "z_{d+1}")
@@ -57,4 +55,8 @@ plot(sliceout.x[:,1],sliceout.x[:,2])
 
 
 #map(x -> sum(x -> x^2, x), eachrow(sliceout.mu))
-#map(x -> sum(x -> x^2, eigen(x - sqrt(d)I(d)).values), sliceout.sigma)
+#plot(log.(map(x -> sum(x -> x^2, eigen(x - sqrt(d)I(d)).values), sliceout.sigma)))
+
+plot(autocor(sliceout.x[:,1].^2))
+
+cov(sliceout.x[floor(Int64,2/3*10^5):end,:])
